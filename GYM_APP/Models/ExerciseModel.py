bedaf0b12137731c886e42d5df1bad131a6959e3
mariog44 <mg441236@gmail.com>
2024-06-19 08:19:44 -0500

    MODELO DE EJERCICIO. Se agregó el modelo de ejercicio para los entrenamientos en el gimnasio.

---
+++ GYM_APP/Models/ExerciseModel.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class ExerciseModel(models.Model):
+    Id_ExerciseModel = models.AutoField(primary_key=True, db_column='Id_Exercise', default=1)
+    Name_ExerciseModel = models.CharField(max_length=100, db_column='Name_Exercise', default='')
+
+    class Meta:
+        managed = True
+        db_table = 'Exercise'
