e84344afdca56438463f8ab5930b978e5303ff24
mariog44 <mg441236@gmail.com>
2024-06-19 08:12:28 -0500

    MODELO DE DIVISIÓN. Se creo el modelo de División de las rutinas.

---
+++ GYM_APP/Models/SplitModel.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class SplitModel(models.Model):
+    Id_SplitModel = models.AutoField(primary_key=True, db_column='Id_Split', default=1)
+    Name_SplitModel = models.CharField(max_length=50, db_column='Name_Split', default='')
+
+    class Meta:
+        managed = True
+        db_table = 'Split'
