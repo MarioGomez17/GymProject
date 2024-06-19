323f2754c9a578839522d6d766432fb50b9a0134
mariog44 <mg441236@gmail.com>
2024-06-18 21:14:40 -0500

    MODELO DE GÉNERO. Se creo el modelo de Genero para los usuarios.

---
+++ GYM_APP/Models/GenderModel.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class GenderModel(models.Model):
+    Id_GenderModel = models.AutoField(primary_key=True, db_column='Id_Gender', default=1)
+    Name_GenderModel = models.CharField(max_length=50, db_column='Name_Gender', default='')
+
+    class Meta:
+        managed = True
+        db_table = 'Gender'
