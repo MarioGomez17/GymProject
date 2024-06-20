e05667d5d2db98a7f9ac7ea043d2bf2fa7dfb802
mariog44 <mg441236@gmail.com>
2024-06-19 23:28:35 -0500

    IMPORTACIÓN DE LOS MODELOS. Se agrega la importación de los modelos en la configuración de la aplicación Django.

--- GYM_APP/apps.py
+++ GYM_APP/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GymAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GYM_APP'
+
+    def ready(self):
+        import GYM_APP.Models
