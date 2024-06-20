cca8182a70d697d863904705f005222795c19527
mariog44 <mg441236@gmail.com>
2024-06-19 23:22:21 -0500

    MODELO SERIE. Se agregó el modelo de Serie para organizar las series de cada rutina del usuario.

---
+++ GYM_APP/Models/SerieModel.py
@@ -0,0 +1,21 @@
+from django.db import models
+from .ExerciseVariantModel import ExerciseVariantModel
+from .ExerciseTypeModel import ExerciseTypeModel
+from .ExerciseEquipmentModel import ExerciseEquipmentModel
+from .WeightUnitModel import WeightUnitModel
+from .RoutineModel import RoutineModel
+
+class SerieModel(models.Model):
+    Id_SerieModel = models.AutoField(primary_key=True, db_column='Id_Serie', default=1)
+    Name_SerieModel = models.CharField(max_length=50, db_column='Name_Serie', default='')
+    ExerciseVariant_SerieModel = models.ForeignKey(ExerciseVariantModel, on_delete=models.PROTECT, db_column='ExerciseVariant_Serie')
+    ExerciseType_SerieModel = models.ForeignKey(ExerciseTypeModel, on_delete=models.PROTECT, db_column='ExerciseType_Serie')
+    ExerciseEquipment_SerieModel = models.ForeignKey(ExerciseEquipmentModel, on_delete=models.PROTECT, db_column='ExerciseEquipment_Serie')
+    Weight_SerieModel = models.FloatField(db_column='Weight_Serie', default=0)
+    WeightUnit_SerieModel = models.ForeignKey(WeightUnitModel, on_delete=models.PROTECT, db_column='WeightUnit_Serie')
+    Rest_SerieModel = models.FloatField(db_column='Rest_Serie', default=0)
+    Routine_SerieModel = models.ForeignKey(RoutineModel, on_delete=models.PROTECT, db_column='Routine_Serie')
+
+    class Meta:
+        managed = True
+        db_table = 'Serie'
