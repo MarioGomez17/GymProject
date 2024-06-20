61ed8a6b311e762ec097ac1c46de1ef0434db4b3
mariog44 <mg441236@gmail.com>
2024-06-19 22:22:44 -0500

    MODELO DE MÚSCULO. Se cambió el nombre de los atributos del modelo añadiendo "Model" al final.

--- GYM_APP/Models/MuscleModel.py
+++ GYM_APP/Models/MuscleModel.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class MuscleModel(models.Model):
-    Id_Muscle = models.AutoField(primary_key=True, db_column='Id_Muscle', default=1)
-    Name_Muscle = models.CharField(max_length=50, db_column='Name_Muscle', default='')
+    Id_MuscleModel = models.AutoField(primary_key=True, db_column='Id_Muscle', default=1)
+    Name_MuscleModel = models.CharField(max_length=50, db_column='Name_Muscle', default='')
 
     class Meta:
         managed = True
