9d6b20d9d7de91964712258624ac057022e05045
mariog44 <mg441236@gmail.com>
2024-06-19 22:58:52 -0500

    MODELO USUARIO. Se agregó el modelo de usuario del sistema.

---
+++ GYM_APP/Models/UserModel.py
@@ -0,0 +1,19 @@
+from django.db import models
+from .WeightUnitModel import WeightUnitModel
+from .GenderModel import GenderModel
+
+
+class UserModel(models.Model):
+    Id_UserModel = models.AutoField(primary_key=True, db_column='Id_User', default=1)
+    Name_UserModel = models.CharField(max_length=50, db_column='Name_User', default='')
+    LastName_UserModel = models.CharField(max_length=50, db_column='LastName_User', default='')
+    Email_UserModel = models.CharField(max_length=50, db_column='Email_User', default='')
+    Password_UserModel = models.CharField(max_length=50, db_column='Password_User', default='')
+    Age_UserModel = models.IntegerField(db_column='Age_User', default=0)
+    Weight_UserModel = models.FloatField(db_column='Weight_User', default=0)
+    WeightUnit_UserModel = models.ForeignKey(WeightUnitModel, on_delete=models.PROTECT, db_column='WeightUnit_User')
+    Gender_UserModel = models.ForeignKey(GenderModel, on_delete=models.PROTECT, db_column='Gender_User')
+
+    class Meta:
+        managed = True
+        db_table = 'User'
