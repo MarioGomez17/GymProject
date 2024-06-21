fc7598b543509aa1382d30f1e84b28d747e69146
mariog44 <mg441236@gmail.com>
2024-06-21 17:40:13 -0500

    URLS DE UNIDADES DE PESO. Se agregaron las URLS para tener los EndPoints de Unidades de Peso.

---
+++ GYM_APP/Urls/WeightUnitURLS.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from ..Views import WeightUnitViewSet
+
+Router = routers.DefaultRouter()
+Router.register(r'WeightUnit', WeightUnitViewSet)
+
+urlpatterns = [
+    path('', include(Router.urls))
+]
