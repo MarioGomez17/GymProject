657070995755166e786ba1380efb08af94ead4c0
mariog44 <mg441236@gmail.com>
2024-06-21 17:37:31 -0500

    URLS DE EJERCICIO. Se agregaron las URLS para tener los EndPoints de Ejercicio.

---
+++ GYM_APP/Urls/ExerciseURLS.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from ..Views import ExerciseViewSet
+
+Router = routers.DefaultRouter()
+Router.register(r'Exercise', ExerciseViewSet)
+
+urlpatterns = [
+    path('', include(Router.urls))
+]
