af36181cb6a9186cf7bb420eca4c437aefb455a3
mariog44 <mg441236@gmail.com>
2024-06-21 18:12:53 -0500

    EXPORTACIÓN DE LAS URLS. Se exportaron todas las URLS de todos los modelos.

--- GYM_APP/Urls/__init__.py
+++ GYM_APP/Urls/__init__.py
@@ -14,3 +14,23 @@ from . import SplitURLS
 from . import SplitUserURLS
 from . import UserURLS
 from . import WeightUnitURLS
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include(DaySplitURLS)),
+    path('', include(ExerciseEquipmentURLS)),
+    path('', include(ExerciseRoutineTemplateURLS)),
+    path('', include(ExerciseTypeURLS)),
+    path('', include(ExerciseURLS)),
+    path('', include(ExerciseVariantURLS)),
+    path('', include(GenderURLS)),
+    path('', include(MuscleDayURLS)),
+    path('', include(MuscleURLS)),
+    path('', include(RoutineTemplateURLS)),
+    path('', include(RoutineURLS)),
+    path('', include(SerieURLS)),
+    path('', include(SplitURLS)),
+    path('', include(SplitUserURLS)),
+    path('', include(UserURLS)),
+    path('', include(WeightUnitURLS))
+]
