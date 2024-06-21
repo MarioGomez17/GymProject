adc1468ac5ded0ea7b5daf73d887270ce6190a31
mariog44 <mg441236@gmail.com>
2024-06-21 16:39:40 -0500

    VISTA DE EQUIPAMIENTO DEL EJERCICIO. Se agregó la vista para poder administrar el Equipamiento del Ejercicio.

--- GYM_APP/Views/ExerciseEquipmentViewSet.py
+++ GYM_APP/Views/ExerciseEquipmentViewSet.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from ..Models import ExerciseEquipmentModel
 from ..Serializers import ExerciseEquipmentSerializer
 
-class DaySplitViewSet(viewsets.ModelViewSet):
+class ExerciseEquipmentViewSet(viewsets.ModelViewSet):
     queryset = ExerciseEquipmentModel.objects.all()
     serializer_class = ExerciseEquipmentSerializer
